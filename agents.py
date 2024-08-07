bf30a466de0d189119c2427db58c0052d319e33f
lewibs <benjaminsl2000@gmail.com>
2024-08-07 17:20:36 -0400

    it learns but its bad

--- agents.py
+++ agents.py
@@ -5,7 +5,8 @@ from collections import deque
 from enum import Enum
 from pynput import keyboard
 import torch
-from env import MEMORY, K_WATCHING, DEVICE, BATCH, TRAINING_GAMES, MIN_EPSILON, START_EPSILON
+from torch import nn, optim
+from env import MEMORY, K_WATCHING, DEVICE, BATCH, TRAINING_GAMES, MIN_EPSILON, START_EPSILON, GAMMA, LR
 from models import ChaserModule, RunnerModule
 
 def get_epsilon_linear(n_games):
@@ -13,7 +14,7 @@ def get_epsilon_linear(n_games):
     return max(MIN_EPSILON, START_EPSILON - epsilon_decay * n_games)
 
 def random_action(game_state):
-    return torch.tensor([[random.randint(-1, 1), random.randint(-1, 1)]], dtype=torch.float32, device=DEVICE)
+    return torch.tensor([random.randint(-1, 1), random.randint(-1, 1)], dtype=torch.float32, device=DEVICE)
 
 class Agent:
     def __init__(self, color, size, start_pos):
@@ -24,6 +25,8 @@ class Agent:
         self.memory = deque(maxlen=MEMORY)
         self.action = []
         self.model = random_action
+        self.optimizer = None
+        self.criterion = None
 
     def draw(self, display):
         # Draw a red rectangle at position (50, 50) with width 100 and height 100
@@ -41,12 +44,38 @@ class Agent:
         return torch.tensor(1)
 
     def train(self):
+        if self.optimizer == None or self.criterion == None:
+            return
+
         if len(self.memory) > BATCH:
             sample = random.sample(self.memory, BATCH)
         else:
             sample = self.memory
 
-        states, action, rewards, next_states = zip(*sample)
+        state, action, reward, next_state = zip(*sample)
+
+        state = torch.stack(state)
+        action = torch.stack(action)
+        reward = torch.stack(reward)
+        next_state = torch.stack(next_state)
+
+        # 1: predicted Q values with current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(state)):
+            Q_new = reward[idx] + GAMMA * torch.max(self.model(next_state[idx]))
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+    
+        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
+
 
     def game_state(self, engine):
         other_objects = []
@@ -63,7 +92,7 @@ class Agent:
             game_state.append(object[0])
             game_state.append(object[1])
 
-        return torch.tensor([game_state], dtype=torch.float32, device=DEVICE)
+        return torch.tensor(game_state, dtype=torch.float32, device=DEVICE)
 
     def step(self, engine): #TODO reward score
         game_state = self.game_state(engine)
@@ -77,8 +106,9 @@ class Agent:
 
         self.action.append(game_state)
         self.action.append(action)
-        new_x = self.position[0] + action[0][0].item()
-        new_y = self.position[1] + action[0][1].item()
+
+        new_x = self.position[0] + action[0].item()
+        new_y = self.position[1] + action[1].item()
 
         if new_x < self.size:
             new_x = self.size
@@ -160,14 +190,25 @@ class Runner(Agent):
     def __init__(self, color, size, start_pos):
         super().__init__(color, size, start_pos)
         self.model = RunnerModule()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=LR)
+        self.criterion = nn.MSELoss()
 
     def reward(self, engine):
-        return self.game_state(engine)[0][5]
+        if len(self.memory) == 0:
+            return torch.tensor(0, dtype=torch.float32, device=DEVICE)
+        
+        max_dist = math.sqrt(engine.h ** 2 + engine.w ** 2)
+        return self.game_state(engine)[5] / max_dist
     
 class Chaser(Agent):
     def __init__(self, color, size, start_pos):
         super().__init__(color, size, start_pos)
         self.model = ChaserModule()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=LR)
+        self.criterion = nn.MSELoss()
 
     def reward(self, engine):
-        return -self.game_state(engine)[0][5]
+        if len(self.memory) == 0:
+            return torch.tensor(0, dtype=torch.float32, device=DEVICE)
+        max_dist = math.sqrt(engine.h ** 2 + engine.w ** 2)
+        return -self.game_state(engine)[5] / max_dist
